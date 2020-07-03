43b876ad1926256c114a0d00939a78a997e8a788
Hugo BARNAS <hugo.barnas@gmail.com>
2020-07-03 12:43:58 +0200

    Création d'une liste avec info par log

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ function loadDoc() {
       country = JSON.parse(this.responseText);
       country.forEach(element => {
         output += '<span class="border border-secondary p-1 m-1">' + element.name + '</span>'
-        console.log(element.name);
-        global = element;
+        global.push(element);
         countryArray.push(element.name);
         //console.log(countryArray);  
         //console.log(global);
@@ -29,6 +28,10 @@ function countrySelected() {
   //identifiation de l'imput
   country = document.getElementById("inputCountry").value;
   //passage de tous les pays dans le tableau
+  //J'identifie le ul que j'ai crée dans le html au niveau du js
+  let myUl = document.getElementById("zoneTexte");
+  //Je clear dans ma boucle ma recherche à chaque fois que je saisis une lettre.
+  myUl.innerHTML = "";
   for (let i = 0; i < countryArray.length; i++) {
     if (countryArray[i].includes(country)) {
       //intégration de la liste li
@@ -37,14 +40,39 @@ function countrySelected() {
 
       //dans mon nouveau li tu vas mettre le nom du pays qui a été trouvé
       myLi.innerHTML = countryArray[i];
-      //J'identifie le ul que j'ai crée dans le html au niveau du js
-      let myUl = document.getElementById("zoneTexte");
+
       //je dis au js que mon li est associé au ul (myLi est associé à mon myUl)
       myUl.appendChild(myLi);
       //console.log(countryArray[i]);
     }
   };
 }
+
+
+discover = () => {
+  let countryResult = document.getElementById("inputCountry").value;
+  for (i = 0; i < global.length; i++) {
+    if (global[i].name.includes(countryResult)) {
+      console.log("NAME : ==> " + global[i].name);
+      console.log("img : ==> " + global[i].flag);
+      console.log("capital : ==> " + global[i].capital);
+    }
+  }
+
+
+
+  console.log("COUNTRY RESULT => " + countryResult);
+
+
+}
+
+let liCountrySelected = document.getElementsByTagName("li");
+//test de fonctionnement
+console.log(liCountrySelected);
+
+
+
+
 
 
 
